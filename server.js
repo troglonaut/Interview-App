aab6df13743e570345e8b56b0e5d3ea983100c1e
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-23 22:07:59 -0600

    added model and controllers dir added js files in those dirs

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
 'use strict';
-//#####################################################
-//Dependencies:
-//#####################################################
+// Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongojs = require('mongojs');
 var mongoose = require('mongoose');
 var path = require('path')
+var passport = require('passport');
+var session = require('express-session');
 
-var app = express();
-// Database
-var db = mongoose.connect('mongodb://localhost/interviewapp-dev');
-
+// MongoDB
+mongoose.connect('mongodb://localhost/interviewapp-dev');
 mongoose.connection.once('connected', function () {
     console.log('Connected to database')
 });
 
+// Express
+var app = express();
 
-
-
-//#####################################################
-//Port:
-//#####################################################
+// Port
 var port = 3000;
 
-//#####################################################
-//Middelware:
-//#####################################################
+// Middleware
 app.use(express.static('public'));
 
 app.use('/', bodyParser.json());
 app.use('/', cors());
 
-//#####################################################
-//Routes:
-//#####################################################
+// Routes
+app.get('/html', function (req, res) {
+    res.send('working');
+})
 
-
-//#####################################################
-//Starting server:
-//#####################################################
-
+// Start server
 app.listen(port, function () {
     console.log('Im watching you port 3000 @.@');
 });
