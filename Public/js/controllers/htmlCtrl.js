77292d438ffd26bbd7cbb72666557a12fdc1eb0e
CodeMaster7 <italianstalian_2@yahoo.com>
2015-09-04 18:10:55 -0600

    score works selected andswers work getting data back into the view

--- Public/js/controllers/htmlCtrl.js
+++ Public/js/controllers/htmlCtrl.js
@@ -5,16 +5,28 @@ app.controller('htmlCtrl', function ($scope, htmlQuestions, $location){
     
     $scope.questions = htmlQuestions;
     
+    $scope.lastQuestion = false;
+    $scope.finished = false;
+    
     $scope.totalQuestions = $scope.questions.length;
     $scope.thisQuestion = 0;
     $scope.currentQuestion = $scope.questions[$scope.thisQuestion];
     $scope.nextProblem = function(index){
         
+        console.log($scope.thisQuestion);
+        console.log($scope.totalQuestions);
+    
+    if ($scope.thisQuestion >= $scope.totalQuestions) {
+        return false;
+    }
         var obj = {}
         obj.selectedAnswer = $scope.selectedIndex;
         obj.correctAnswer = $scope.currentQuestion.answer;
         obj.question = $scope.thisQuestion;
+        
+        $scope.selectedIndex = null;
         $scope.userAnwers.push(obj)
+        
         console.log("Selected Inx: ", $scope.selectedIndex);
         console.log(index);
         console.log($scope.userAnwer);
@@ -24,16 +36,34 @@ app.controller('htmlCtrl', function ($scope, htmlQuestions, $location){
         console.log("thisQuestion: ", $scope.thisQuestion)
             $scope.currentQuestion = $scope.questions[$scope.thisQuestion];
         $scope.val = undefined;
+        
+        if ($scope.thisQuestion === $scope.totalQuestions) {
+        $scope.lastQuestion = true;
+        return false;
+        }
     }
     console.log("Questions: ", $scope.questions)
     console.log("Current Question: ", $scope.currentQuestion)
     console.log("This Question: ", $scope.thisQ1uestion)
-//    console.log("Questins: ", $scope.questions)
-//    console.log("Questins: ", $scope.questions)
-//    console.log("Questins: ", $scope.questions)
+   
 $scope.onChange = function(idx) {
     $scope.selectedIndex = idx;
     console.log($scope.selectedIndex);
   };
+    
+    $scope.calcResult = function () {
+        var numRight = 0
+        var tempArray = $scope.userAnwers;
+        
+        //loop through userAnswers and calculate numer right
+        for (var i = 0; i < tempArray.length; i++ ) {
+            if (tempArray[i].selectedAnswer === tempArray[i].correctAnswer) {
+                numRight++;
+            }
+        }
+        $scope.questionsCurrect = numRight;
+        //finished to true 
+        $scope.finished = true;
+    }
 });
 
